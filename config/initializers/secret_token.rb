0705384b204c729f953c9acfdc20536fb32f1294
ekta-verma <Ekta_Verma@external.mckinsey.com>
2013-10-29 14:25:52 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmailAttachment::Application.config.secret_token = '6cbfeda3a12582484db13dc63a52ffd998a8fe53ae041f6c8f06d03241c2436ed7437c51dd51936e077e5bf1a8e26d4c5d7c3c38fe3dd30917a872f38a59358f'
